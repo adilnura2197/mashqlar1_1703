0acb2d86f461f0b75835ba52a7695e25ff2e5ca0
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-03-16 16:12:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#1
+a = [10, 20, 30, 40]
+print(a)
+
+b = len(a)
+print(f"a ning uzunligi: {b}")
+
+
+#2
+a = [5, 8, 12, 7]
+print(a)
+
+
+#3
+mevalar = ["olma", "anor", "uzum"]
+print(mevalar)
+
+
+#4
+sonlar = [1, 2, 3, 4, 5]
+print(sonlar)
+
+print(sonlar[-1])
+
+
+#5
+a = [11, 22, 33]
+print(a)
+
+print(a[0])
